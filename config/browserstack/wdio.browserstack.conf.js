b921407156566e432675c798052c57945b126cd9
raphaelpalhano <raphael1angel@hotmail.com>
2022-04-19 19:54:23 -0300

    configurando o report unit para teste runs

--- config/browserstack/wdio.browserstack.conf.js
+++ config/browserstack/wdio.browserstack.conf.js
@@ -75,6 +75,15 @@ exports.config = {
 
         }
       ],
+      [
+        'browserstack', {
+          reporterOptions: {
+            browserstack: {
+              outputDir: "./"
+            }
+          },
+        }
+      ],
 
     ],
     // =====
