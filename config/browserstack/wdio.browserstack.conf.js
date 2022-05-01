c451b3dc0d3f7af8e40dc454d1ad03212289168c
raphaelpalhano <raphael1angel@hotmail.com>
2022-05-01 14:42:43 -0300

    change env

--- config/browserstack/wdio.browserstack.conf.js
+++ config/browserstack/wdio.browserstack.conf.js
@@ -2,7 +2,7 @@ const { generate } = require('multiple-cucumber-html-reporter');
 const browserstack = require('browserstack-local');
 const ScreenManagerMobile = require('../../src/components/native/ScreenManagerMobile');
 const AppCapabilities = require('../utils/AppCapabilities');
-require('dotenv').config({ path: './env/.env' })
+require('dotenv').config({ path: 'config/env/.env' })
 
 
 exports.config = {
